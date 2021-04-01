f7a0b6444380d1948a2ba345356d00f9afae8b7e
OveR1st <315overdose@gmail.com>
2021-04-01 17:55:16 +0300

    Mod materials fix for gh-pages

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -4,7 +4,7 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/script.js'),
+    entry: path.resolve(__dirname, '../src/script28(Modified Material).js'),
     output:
     {
         filename: 'bundle.[contenthash].js',
@@ -90,7 +90,20 @@ module.exports = {
                     'raw-loader',
                     // 'glslify-loader'
                 ]
-            }
+            },
+            {
+                test: /\.(glb|gltf)$/,
+                use:
+                    [
+                        {
+                            loader: 'file-loader',
+                            options:
+                                {
+                                    outputPath: 'assets/models/'
+                                }
+                        }
+                    ]
+            },
         ]
     }
 }
